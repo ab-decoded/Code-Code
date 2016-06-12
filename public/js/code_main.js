7024310605dc26e05fb69730cd72ad4875c99685
Abhinav Bansal <abhinavbansal0@gmail.com>
2016-06-12 20:10:53 +0530

    Socket IO

--- public/js/code_main.js
+++ public/js/code_main.js
@@ -1,5 +1,21 @@
-var textArea = document.getElementById('codeArea');
-var editor = CodeMirror.fromTextArea(textArea,{
-	lineNumbers:true
+$(document).ready(function(){
+	CodeMirror.modeURL = "../components/codemirror/mode/%N/%N.js";
+	$('#mode-select').dropdown();
+	var textArea = document.getElementById('codeArea');
+	var editor = CodeMirror.fromTextArea(textArea,{
+		lineNumbers:true
+	});
+	editor.getDoc().setValue('var msg = "Hi";');
+	$('#mode-select').on('change',function(event){
+		var info=CodeMirror.findModeByName(event.target.value);
+		if(info){
+			mode=info.mode;
+			spec=info.mime;
+		}
+		console.log(info);
+		if(mode){
+			editor.setOption("mode",spec);
+			CodeMirror.autoLoadMode(editor,mode);
+		}
+	});
 });
-editor.getDoc().setValue('var msg = "Hi";');
